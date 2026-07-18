f4e8d8bee29c6873f0bacb0ce117ef554649b496
BabaYagashka <virajshinde2005@gmail.com>
2026-07-18 22:34:09 +0530

    Vite + React + TypeScript + Tailwind configured

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <h1 className="text-4xl font-bold text-white">Tailwind is working</h1>
+    </div>
+  );
+}
+
+export default App;
